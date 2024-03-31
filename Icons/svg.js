75502dab2e3b21d20e8e6f0acf289e4581db934e
vk <vijkessshi@gmail.com>
2024-03-31 19:26:16 +0530

    first commit

---
+++ Icons/svg.js
@@ -0,0 +1,8 @@
+import MenuIcon from "./menu.svg"
+import CloseIcon from "./close-icon.svg"
+
+let icons = {
+    MenuIcon,
+    CloseIcon
+}
+export default icons;
---
+++ Images/image.js
@@ -0,0 +1,62 @@
+import Logo from "./logo.png"
+import SLogo from "./Slogo.png"
+import SLogorbg from "./Slogo-rbg.png"
+import SmLogo from "./smlogo.png"
+import MainBanner from "./main-banner.webp"
+import UnityBankrbg from './unityfinance-rbg.png'
+import awardsframe from  './awards-frame.svg'
+import awardstree from  './awards-tree.svg'
+import equitas from './equitas-rbg.png'
+import finnable from './finnable-rbg.png'
+import ugro from './UGRO-rbg.png'
+import adhaarRbg from './Aadhar-rbg.png'
+import landingkart from './landing-kart-rbg.png'
+import neomain from './Neomain-rbg.png'
+import bajaj from './bajaj-finance6835.jpg'
+import bandhanbank from './bandhan-bank3983.jpg'
+import BOB from './BOB.png'
+import faircent from './faircent.jpg'
+import ashv from './ashv.jpg'
+import dcb from './dcb.png'
+import yesbank from './yesbank.png'
+import Au from './Au.png'
+import Neomain from './Neomain.jpg'
+import Axis from './Axis.jpg'
+
+import Fullerton from './Fullerton.png'
+import HDFC from './HDFC.png'
+import icici from './icici.png'
+import Hero from './Hero.png'
+import idfc from './idfc.jpg'
+import iifl from './iifl.jpg'
+
+import incred from './incred.jpeg'
+import indusland from './indusland.jpg'
+import jana from './jana.png'
+import L_T from './L&T.png'
+import paysense from './paysense.jpg'
+import pnb from './pnb.jpg'
+import ujjivan from './ujjivan.jpg'
+import utkarsh from './utkarsh.jpg'
+
+
+import personal from '../Images/loan/personal.png'
+import construction from '../Images/loan/construction.png'
+import business from '../Images/loan/business.png'
+import property from '../Images/loan/property.png'
+
+
+let images = {
+    property,personal,business,construction,
+    equitas,finnable,ugro,landingkart,adhaarRbg,neomain,bandhanbank,BOB,faircent,bajaj,Au,dcb,yesbank,ashv,Neomain,Axis,Fullerton,HDFC,icici,Hero,idfc,iifl,jana,L_T,indusland,incred,paysense,pnb,utkarsh,ujjivan,
+    Logo,
+    SLogo,
+    SLogorbg,
+    SmLogo,
+    MainBanner,
+    UnityBankrbg,
+    awardsframe,
+    awardstree
+
+}
+export default images;
